dda48a98a838b0d821522872fa08a72cce266033
briantruan <briantruan@gmail.com>
2021-12-31 16:27:43 -0500

    version 1.1 - adds persistence

--- src/main/java/io/github/rentedpanda/barthybot/commands/ThoughtsCommand.java
+++ src/main/java/io/github/rentedpanda/barthybot/commands/ThoughtsCommand.java
@@ -4,12 +4,10 @@ import com.google.gson.Gson;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Random;
 
 public class ThoughtsCommand extends ListenerAdapter {
-
     @Override
     public void onSlashCommand(SlashCommandEvent event) {
         if (event.getName().equalsIgnoreCase("thoughts")) {
@@ -25,9 +23,26 @@ public class ThoughtsCommand extends ListenerAdapter {
 
     private String getThought() throws FileNotFoundException {
         Gson gson = new Gson();
-        String[] thoughts = gson.fromJson(new FileReader("src/main/resources/thoughts.json"), String[].class);
+        InputStream inputStream = getFileFromResourceAsStream("thoughts.json");
+        Reader thoughtsJson = new InputStreamReader(inputStream);
+        String[] thoughts = gson.fromJson(thoughtsJson, String[].class);
         int index = getRandomInt(0, thoughts.length - 1);
         return thoughts[index];
+    }
+
+    // imported from https://mkyong.com/java/java-read-a-file-from-resources-folder/
+
+    private InputStream getFileFromResourceAsStream(String fileName) {
+        // The class loader that loaded the class
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+
+        // the stream holding the file content
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return inputStream;
+        }
     }
 
     private int getRandomInt(int min, int max) {
@@ -35,5 +50,4 @@ public class ThoughtsCommand extends ListenerAdapter {
         Random random = new Random();
         return random.nextInt(max - min) + min;
     }
-
 }
