857054ef41ab840a3264c96f5ae771c9ea6ed71e
briantruan <briantruan@gmail.com>
2021-12-31 12:00:26 -0500

    refined ThoughtsCommand.java to be more editable

--- src/main/java/io/github/rentedpanda/barthybot/commands/ThoughtsCommand.java
+++ src/main/java/io/github/rentedpanda/barthybot/commands/ThoughtsCommand.java
@@ -1,44 +1,33 @@
 package io.github.rentedpanda.barthybot.commands;
 
+import com.google.gson.Gson;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Random;
 
 public class ThoughtsCommand extends ListenerAdapter {
-    private final int MAX_CASES = 8;
 
     @Override
     public void onSlashCommand(SlashCommandEvent event) {
         if (event.getName().equalsIgnoreCase("thoughts")) {
-            int indexOfThought = getRandomInt(0, MAX_CASES);
-            String thought = getThought(indexOfThought);
+            String thought = "nein!";
+            try {
+                thought = getThought();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
             event.reply(thought).queue();
         }
     }
 
-    private String getThought(int index) {
-        assert (0 <= index && index <= MAX_CASES);
-        switch (index) {
-            case 0:
-                return "real";
-            case 1:
-                return "pain.";
-            case 2:
-                return "balls.";
-            case 3:
-                return "balls in my face.";
-            case 4:
-                return "not right.";
-            case 5:
-                return "isn't that special?";
-            case 6:
-                return "SQUID GAME";
-            case 7:
-                return "ey. ey. ey.";
-            default:
-                return "nein.";
-        }
+    private String getThought() throws FileNotFoundException {
+        Gson gson = new Gson();
+        String[] thoughts = gson.fromJson(new FileReader("src/main/resources/thoughts.json"), String[].class);
+        int index = getRandomInt(0, thoughts.length - 1);
+        return thoughts[index];
     }
 
     private int getRandomInt(int min, int max) {
@@ -46,4 +35,5 @@ public class ThoughtsCommand extends ListenerAdapter {
         Random random = new Random();
         return random.nextInt(max - min) + min;
     }
+
 }
