3c96dcbc185ab3d21c92e6458c1704863eab35c5
briantruan <briantruan@gmail.com>
2021-12-31 20:08:33 -0500

    prepare for heroku

--- src/main/java/io/github/rentedpanda/barthybot/Barthybot.java
+++ src/main/java/io/github/rentedpanda/barthybot/Barthybot.java
@@ -1,8 +1,6 @@
 package io.github.rentedpanda.barthybot;
 
-import io.github.rentedpanda.barthybot.commands.BoomCommand;
-import io.github.rentedpanda.barthybot.commands.RealCommand;
-import io.github.rentedpanda.barthybot.commands.ThoughtsCommand;
+import io.github.rentedpanda.barthybot.commands.*;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
@@ -16,12 +14,16 @@ public class Barthybot {
     public static void main(String[] arguments) throws LoginException {
         JDA api = JDABuilder.createLight(BOT_TOKEN, Collections.emptyList())
                 .addEventListeners(new BoomCommand())
+                .addEventListeners(new MemeCommand())
                 .addEventListeners(new RealCommand())
+                .addEventListeners(new SquidwardCommand())
                 .addEventListeners(new ThoughtsCommand())
                 .setActivity(Activity.playing("real."))
                 .build();
         api.upsertCommand("boom", "BOOM").queue();
+        api.upsertCommand("meme", "Acquire a meme from Barhty's fine selections").queue();
         api.upsertCommand("real", "real").queue();
+        api.upsertCommand("squidward", "SQUID GAME").queue();
         api.upsertCommand("thoughts", "What is Barthy thinking about?").queue();
     }
 }
---
+++ src/main/java/io/github/rentedpanda/barthybot/commands/SquidwardCommand.java
@@ -0,0 +1,6 @@
+package io.github.rentedpanda.barthybot.commands;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class SquidwardCommand extends ListenerAdapter {
+}
